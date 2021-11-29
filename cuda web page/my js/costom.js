91af69ca27e6e278cc1a7feb0bde53d62a3407c6
hamidul1999 <95200356+hamidul1999@users.noreply.github.com>
2021-11-29 09:44:12 +0600

    psd to html

---
+++ cuda web page/my js/costom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+var mixer = mixitup('.mixx');
+
+
+
+
+
+});
+
+var bar = document.querySelector('.manu-bar');
+var navbaropen = document.querySelector('.navbaropen');
+var navbaropen1 = document.querySelector('.navbaropen');
+var navbarclose= document.querySelector('.fa-times-circle');
+
+var navbaropen111 = document.querySelector('.navbaropen111');
+function openbar(){
+
+    navbaropen111.style.width="100%";
+    navbaropen.style.display="block";
+    navbarclose.style.display="block";
+   
+  
+
+
+
+
+}
+
+function closebar(){
+
+    navbaropen111.style.width="00%";
+    navbaropen.style.display="none";
+    
+
+}
